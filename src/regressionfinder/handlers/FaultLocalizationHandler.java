81a63602ba44b471285782e5740ac0f4baf42ecc
marinan87 <marina.nekrassova@ttu.ee>
2017-03-10 11:39:24 +0200

    draft implementation of filtering 'safe' changes + improved displaying of results

--- src/regressionfinder/handlers/FaultLocalizationHandler.java
+++ src/regressionfinder/handlers/FaultLocalizationHandler.java
@@ -2,6 +2,7 @@ package regressionfinder.handlers;
 
 import java.io.File;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
@@ -16,7 +17,12 @@ import org.eclipse.ui.texteditor.ITextEditor;
 import ch.uzh.ifi.seal.changedistiller.ChangeDistiller;
 import ch.uzh.ifi.seal.changedistiller.ChangeDistiller.Language;
 import ch.uzh.ifi.seal.changedistiller.distilling.FileDistiller;
+import ch.uzh.ifi.seal.changedistiller.model.classifiers.SignificanceLevel;
+import ch.uzh.ifi.seal.changedistiller.model.entities.Delete;
+import ch.uzh.ifi.seal.changedistiller.model.entities.Insert;
+import ch.uzh.ifi.seal.changedistiller.model.entities.Move;
 import ch.uzh.ifi.seal.changedistiller.model.entities.SourceCodeChange;
+import ch.uzh.ifi.seal.changedistiller.model.entities.Update;
 import dd.DeltaDebug;
 import regressionfinder.testrunner.JUnitTestHarness;
 import regressionfinder.utils.JavaModelHelper;
@@ -33,14 +39,16 @@ public class FaultLocalizationHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
+		List<SourceCodeChange> failureInducingChanges = null;
 		try {
 			ICompilationUnit sourceCU = JavaModelHelper.getCompilationUnit(VERSION_BEFORE_REGRESSION, LOCALIZATION_SOURCE);
 			ICompilationUnit regressionCU = JavaModelHelper.getCompilationUnit(VERSION_WITH_REGRESSION, LOCALIZATION_SOURCE);
 
 			List<SourceCodeChange> allChanges = extractSourceCodeChanges(sourceCU, regressionCU);
+			List<SourceCodeChange> filteredChanges = excludeSafeChanges(allChanges);
 						
 			JUnitTestHarness testHarness = new JUnitTestHarness(sourceCU);
-			List<SourceCodeChange> failureInducingChanges = DeltaDebug.ddmin(allChanges, testHarness);
+			failureInducingChanges = DeltaDebug.ddmin(filteredChanges, testHarness);
 			System.out.println(failureInducingChanges);
 			
 			applyFailureInducingChanges(sourceCU, failureInducingChanges);
@@ -50,8 +58,14 @@ public class FaultLocalizationHandler extends AbstractHandler {
 			return null;
 		} 
 		
-		displayDoneDialog(event);
+		displayDoneDialog(event, failureInducingChanges);
 		return null;
+	}
+
+	private List<SourceCodeChange> excludeSafeChanges(List<SourceCodeChange> allChanges) {
+		return allChanges.stream()
+				.filter(change -> change.getChangeType().getSignificance() != SignificanceLevel.NONE)
+				.collect(Collectors.toList());
 	}
 
 	private List<SourceCodeChange> extractSourceCodeChanges(ICompilationUnit originalCU, ICompilationUnit regressionCU) throws JavaModelException {
@@ -83,8 +97,42 @@ public class FaultLocalizationHandler extends AbstractHandler {
 		textEditor.selectAndReveal(startPosition, length);
 	}
 	
-	private void displayDoneDialog(ExecutionEvent event) throws ExecutionException {
+	private void displayDoneDialog(ExecutionEvent event, List<SourceCodeChange> failureInducingChanges) throws ExecutionException {
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		MessageDialog.openInformation(window.getShell(), "RegressionFinder", "Done");
+		MessageDialog.openInformation(window.getShell(), "RegressionFinder", getStringRepresentation(failureInducingChanges));
 	}
+
+	private String getStringRepresentation(List<SourceCodeChange> failureInducingChanges) {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Regression is caused by the following changes:\r\n\r\n");
+		
+		for (SourceCodeChange change : failureInducingChanges) {
+			if (change instanceof Insert) {
+				Insert insert = (Insert) change;
+				builder.append("Inserted\t");
+				builder.append(insert.getChangedEntity().getUniqueName());
+			} else if (change instanceof Update) {
+				Update update = (Update) change;
+				builder.append("Updated\t");
+				builder.append(update.getChangedEntity().getUniqueName());
+				builder.append(" ===> ");
+				builder.append(update.getNewEntity().getUniqueName());
+			} else if (change instanceof Delete) {
+				Delete delete = (Delete) change;
+				builder.append("Deleted\t");
+				builder.append(delete.getChangedEntity().getUniqueName());
+			} else if (change instanceof Move) {
+				Move move = (Move) change;
+				builder.append("Moved\t");
+				builder.append(String.format("entity %s in parent %s", move.getChangedEntity().getUniqueName(), move.getParentEntity().getUniqueName()));
+				builder.append(String.format(", now becomes entity %s in parent %s", move.getNewEntity().getUniqueName(), move.getNewParentEntity().getUniqueName()));
+			}
+			
+			builder.append("\r\n");
+		}
+		
+		return builder.toString();
+	}
+	
+	
 }
